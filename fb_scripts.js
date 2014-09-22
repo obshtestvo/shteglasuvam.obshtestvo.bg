faf4fd89389c24771af0e49541966d66d218de0e
Bobby Delev <b.delev@pafty.net>
2014-09-22 14:23:11 +0300

    Voted check with cookies

--- fb_scripts.js
+++ fb_scripts.js
@@ -24,8 +24,13 @@ window.fbAsyncInit = function() {
   FB.getLoginStatus(function(response) {
     if(response.status === 'connected') {
         console.log("Connected");
-        $("#badge").show();
-
+        var voted = docCookies.getItem("voted");
+        if (voted) {
+          $("#badge").show();  
+        }else{
+          $("#login_vote").show();
+        };
+        
         getFaces();
     }else if(response.status === 'not_authorized'){
       console.log("not_authorized");
@@ -41,6 +46,69 @@ window.fbAsyncInit = function() {
   });
 };    //FB Async Init    
 
+
+/*\
+|*|
+|*|  :: cookies.js ::
+|*|
+|*|  A complete cookies reader/writer framework with full unicode support.
+|*|
+|*|  Revision #1 - September 4, 2014
+|*|/
+|*|  https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
+|*|
+|*|  This framework is released under the GNU Public License, version 3 or later.
+|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
+|*|
+|*|  Syntaxes:
+|*|
+|*|  * docCookies.setItem(name, value[, end[, path[, domain[, secure]]]])
+|*|  * docCookies.getItem(name)
+|*|  * docCookies.removeItem(name[, path[, domain]])
+|*|  * docCookies.hasItem(name)
+|*|  * docCookies.keys()
+|*|
+\*/
+
+var docCookies = {
+  getItem: function (sKey) {
+    if (!sKey) { return null; }
+    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+  },
+  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+    var sExpires = "";
+    if (vEnd) {
+      switch (vEnd.constructor) {
+        case Number:
+          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+          break;
+        case String:
+          sExpires = "; expires=" + vEnd;
+          break;
+        case Date:
+          sExpires = "; expires=" + vEnd.toUTCString();
+          break;
+      }
+    }
+    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    return true;
+  },
+  removeItem: function (sKey, sPath, sDomain) {
+    if (!this.hasItem(sKey)) { return false; }
+    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+    return true;
+  },
+  hasItem: function (sKey) {
+    if (!sKey) { return false; }
+    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+  },
+  keys: function () {
+    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+    for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+    return aKeys;
+  }
+};
 
 function increment(i){
   $('#retroclockbox_counter').flipcountdown({size:'sm',
@@ -88,6 +156,7 @@ function postDialog(){
       url: "http://shteglasuvam.obshtestvo.bg/counter.php?m=increment"
     }).done(function(data){
       increment(data);
+      docCookies.setItem("voted", "true", Infinity);
       document.location.reload(true); 
     });
   });
@@ -135,12 +204,12 @@ function who(){
     FB.getLoginStatus(function(response) {
       if (response.status === 'connected') {
                  
-      console.log("Connected");
+      // console.log("Connected");
       getFaces();
 
     }else if(response.status === 'not_authorized'){
           
-          console.log("Not Autorized");
+          // console.log("Not Autorized");
           getEmptyFaces();
           FB.login(function(response){
             if (response.status === 'connected') {
@@ -150,7 +219,7 @@ function who(){
               
     } else {
 
-      console.log("NOT CONNECTED");
+      // console.log("NOT CONNECTED");
       $("#faces").text("");
       getEmptyFaces();
       FB.login(function(){
