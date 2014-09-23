7c057732a8968211a5865e2236f4e6d708da5f30
Vasil Kolev <vasil@ludost.net>
2014-09-23 21:27:05 +0300

    A small fix, to work around a race condition
    The whole thing is very very bad, and should not get into production
    anyway, but at least this version doesn't have a simple race condition
    and doesn't open the file twice for recording anything. I strongly
    recommend using a database for this.

--- counter.php
+++ counter.php
@@ -3,36 +3,37 @@
 
 if($_REQUEST['m'] == 'increment'){
 	// Open the file for reading 
-	$fp = fopen("cnt.dat", "r"); 
-
-	// Get the existing count 
-	$count = fread($fp, 1024); 
-
-	// Close the file 
-	fclose($fp); 
-
-	// Add 1 to the existing count 
-	$count = $count + 1;
-
-	// Reopen the file and erase the contents 
 	$fp = fopen("cnt.dat", "w"); 
 
-	// Write the new count to the file 
+	// Lock the file, to prevent a race condition
+	flock($fp, LOCK_EX);
+
+	// Get the existing count
+	$count = (int) fread($fp, 1024);
+
+	// Add 1 to the existing count
+	$count++;
+
+	// Write the new count to the file
+	ftruncate($fp, 0);
+	fseek($fp, 0);
 	fwrite($fp, $count); 
 
-	// Close the file 
+	// Unlock and close the file
+	flock($fp, LOCK_UN);
 	fclose($fp); 
 	echo $count;
 	
 }elseif ($_REQUEST['m'] == 'check'){
 
+	// No lock needed here, writing a single page is an atomic op
 	$fp = fopen("cnt.dat", "r"); 
 
-	// Get the existing count 
-	$count = fread($fp, 1024); 
+	// Get the existing count
+	$count = (int) fread($fp, 1024);
 
-	// Close the file 
-	fclose($fp); 
+	// Close the file
+	fclose($fp);
 
 	echo $count;
 
